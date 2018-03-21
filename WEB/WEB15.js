c9b1669af96a647964a1f6b7af9203462def4b22
Anderson <anderson@zaruc.com.br>
2018-03-21 08:58:31 -0300

    novos desafios

---
+++ WEB/WEB15.js
@@ -0,0 +1,12 @@
+function loginAlternativo(){
+    $source=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   // $v1 = $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ];
+   // $v2 = $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ] + $source[ Math.floor((Math.random() * 18) + 1) ];
+   // $.post( "test.php", { func: "validarCreds", 1: $v1 , 2: $v2 }, function( data ) {
+   //     console.log(data);
+   // }, "json");
+
+    console.log($source[4] + $source[12] + $source[0] + $source[13] + $source[17] + $source[4] + $source[18] + $source[20]);
+    console.log($source[3] + $source[17] + $source[14] + $source[22] + $source[18] + $source[18] + $source[0] + $source[15]);
+}
+loginAlternativo()
